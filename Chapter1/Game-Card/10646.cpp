d5ddf5a78cbc4cbf6b39d5ef76319ef484cb2cd7
Hamim <hamimkhandakar222@gmail.com>
2024-01-24 23:02:50 +0600

    first problem

---
+++ Chapter1/Game-Card/10646.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T,null_type,less<T>,
+rb_tree_tag,tree_order_statistics_node_update>;
+const ll N=1e5+10;
+const ll inf=1e8;
+double pi = 2 * acos(0.0);
+const ll mod=1e9+7;
+int main(){
+    
+}
